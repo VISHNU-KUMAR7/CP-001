b955a067fc3de786710bd9ef78b40dcf580045d7
Vishnu Kumar <vklucifer80@gmail.com>
2022-07-02 02:59:02 +0530

    Basic userAction is done

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,5 +1,12 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../redux/action/userAction";
 
 export default function Login() {
-  return <div>Login</div>;
+  const dispatch = useDispatch();
+  return (
+    <>
+      Login<button onClick={() => dispatch(login())}>Login</button>
+    </>
+  );
 }
