c2fec0fa44611023ea51defb10a8af39345a586b
Vishnu Kumar <vklucifer80@gmail.com>
2022-07-02 00:07:20 +0530

    Basic lazy loading is applied on routes

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -1,30 +1,111 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import Dashboard from "../components/Dashboard";
-import EditIssue from "../components/EditIssue";
-import AddIssue from "../components/AddIssue";
-import ViewIssue from "../components/ViewIssue";
-import DetailIssue from "../components/DetailIssue";
-import Register from "../components/Register";
-import Login from "../components/Login";
-import Profile from "../components/Profile";
-import Help from "../components/Help";
-import About from "../components/About";
+
+const Dashboard = lazy(() => import("../components/Dashboard"));
+const EditIssue = lazy(() => import("../components/EditIssue"));
+const AddIssue = lazy(() => import("../components/AddIssue"));
+const ViewIssue = lazy(() => import("../components/ViewIssue"));
+const DetailIssue = lazy(() => import("../components/DetailIssue"));
+const Register = lazy(() => import("../components/Register"));
+const Login = lazy(() => import("../components/Login"));
+const Profile = lazy(() => import("../components/Profile"));
+const Help = lazy(() => import("../components/Help"));
+const About = lazy(() => import("../components/About"));
 
 export default function routes() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/editIssue" element={<EditIssue />} />
-        <Route path="/addIssue" element={<AddIssue />} />
-        <Route path="/viewIssue" element={<ViewIssue />} />
-        <Route path="/detailIssue" element={<DetailIssue />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/help" element={<Help />} />
-        <Route path="/About" element={<About />} />
+        <Route
+          path="/"
+          element={
+            <Suspense fallback={<span>Please Wait....</span>}>
+              {" "}
+              <Dashboard />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/editIssue"
+          element={
+            <Suspense fallback={<span>Please Wait....</span>}>
+              {" "}
+              <EditIssue />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/addIssue"
+          element={
+            <Suspense fallback={<span>Please Wait....</span>}>
+              {" "}
+              <AddIssue />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/viewIssue"
+          element={
+            <Suspense fallback={<span>Please Wait....</span>}>
+              {" "}
+              <ViewIssue />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/detailIssue"
+          element={
+            <Suspense fallback={<span>Please Wait....</span>}>
+              {" "}
+              <DetailIssue />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <Suspense fallback={<span>Please Wait....</span>}>
+              {" "}
+              <Register />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <Suspense fallback={<span>Please Wait....</span>}>
+              {" "}
+              <Login />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <Suspense fallback={<span>Please Wait....</span>}>
+              {" "}
+              <Profile />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/help"
+          element={
+            <Suspense fallback={<span>Please Wait....</span>}>
+              {" "}
+              <Help />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/About"
+          element={
+            <Suspense fallback={<span>Please Wait....</span>}>
+              {" "}
+              <About />
+            </Suspense>
+          }
+        />
       </Routes>
     </>
   );
