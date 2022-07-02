e9b985bb7d4bd694acd3a6b6e00fc6729eb43754
Vishnu Kumar <vklucifer80@gmail.com>
2022-07-02 13:59:50 +0530

    Saga is config with basic code👶

--- frontend/src/redux/action/userAction.js
+++ frontend/src/redux/action/userAction.js
@@ -1,3 +1,5 @@
+
 export const login = (data) => {
+ 
   return { type: "LOGIN", data };
 };
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./reducer/rootReducer";
-const dummyReducer = () => {
-  return 100;
-};
-const store = configureStore({ reducer: rootReducer });
-
+import userSaga from "./saga/userSaga";
+import createSagaMiddleware from "redux-saga";
+// const dummyReducer = () => {
+//   return 100;
+// };
+const sagaMiddleware = createSagaMiddleware();
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: () => [sagaMiddleware],
+});
+sagaMiddleware.run(userSaga);
 export default store;
