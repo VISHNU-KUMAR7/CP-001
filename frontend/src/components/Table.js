d1a9e2214f4667f6fa537044451e17a6419315b3
Vishnu Kumar <vklucifer80@gmail.com>
2022-07-07 11:50:36 +0530

    new field added in userSchema

---
+++ frontend/src/components/Table.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { delIssue } from "../redux/action/issueAction";
+import { Link, useNavigate } from "react-router-dom";
+
+export default function Table(props) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { _id, description, severity, status } = props;
+  const deleteIssue = async (e) => {
+    if (window.confirm(`R u want to delet ${_id}`)) {
+      dispatch(delIssue({ _id }));
+      navigate("/viewIssue");
+    }
+  };
+  return (
+    <>
+      <tr>
+        <th scope="row">{_id}</th>
+        <td>{description}</td>
+        <td>{severity}</td>
+        <td>{status}</td>
+        <td>
+          <div className="row">
+            <div className="col-md-5">
+              <Link
+                to="/editIssue"
+                className="card-link text-success d-inline-block"
+                state={{ _id, description, severity, status }}
+              >
+                <ion-icon name="create-outline"></ion-icon>
+              </Link>
+            </div>
+            <div className="col-md-5">
+              <Link
+                to="#"
+                className="card-link text-danger d-inline-block"
+                onClick={(e) => {
+                  deleteIssue(e);
+                }}
+              >
+                <ion-icon name="trash-outline"></ion-icon>
+              </Link>
+            </div>
+          </div>
+        </td>
+      </tr>
+    </>
+  );
+}
