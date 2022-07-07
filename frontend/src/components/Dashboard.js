eb639619b4959e57ce06e57c9b1c66d9ca8bd04f
Vishnu Kumar <vklucifer80@gmail.com>
2022-07-08 03:27:45 +0530

    try to add graph but it show attitude.👿👿

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { getTotalIssueByUser } from "../redux/action/userAction";
-
+import { ResponsiveContainer, PieChart, Pie } from "recharts";
 export default function Dashboard() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -18,15 +18,68 @@ export default function Dashboard() {
     setTotalIssue(TotalIssue);
   }, [TotalIssue]);
 
-  const logout = (e) => {
-    console.log(
-      "Logout is not working for all. It will work only when you refresh ur page once or do dome activiy.",
-      localStorage.getItem("eMail")
-    );
-    localStorage.removeItem("eMail");
-    console.log(localStorage.getItem("eMail"));
-    navigate("/login");
-  };
+  const data01 = [
+    {
+      name: "Group A",
+      value: 400,
+    },
+    {
+      name: "Group B",
+      value: 300,
+    },
+    {
+      name: "Group C",
+      value: 300,
+    },
+    {
+      name: "Group D",
+      value: 200,
+    },
+    {
+      name: "Group E",
+      value: 278,
+    },
+    {
+      name: "Group F",
+      value: 189,
+    },
+  ];
+  const data02 = [
+    {
+      name: "Group A",
+      value: 2400,
+    },
+    {
+      name: "Group B",
+      value: 4567,
+    },
+    {
+      name: "Group C",
+      value: 1398,
+    },
+    {
+      name: "Group D",
+      value: 9800,
+    },
+    {
+      name: "Group E",
+      value: 3908,
+    },
+    {
+      name: "Group F",
+      value: 4800,
+    },
+  ];
+
+  // const logout = (e) => {
+  //   console.log(
+  //     "Logout is not working for all. It will work only when you refresh ur page once or do dome activiy.",
+  //     localStorage.getItem("eMail")
+  //   );
+  //   localStorage.removeItem("eMail");
+  //   console.log(localStorage.getItem("eMail"));
+  //   navigate("/login");
+  // };
   return (
     <>
       <div className="row border border-info border-2 rounded  px-lg-5 py-auto px-md-3 px-sm-2 justify-content-center text-bg-secondary">
@@ -38,10 +91,22 @@ export default function Dashboard() {
           Total Issue: {totalIssue.totIssues}
         </div>
       </div>
-      <div className="row ">
-        <div className="col-md-8">
-
+      <div className="row justify-content-evenly ">
+        <div className="col-md-3 border border-info border-3 rounded h-100">
+          <PieChart style={{ position: "relative" }} width={250} height={250}>
+            <Pie
+              data={data02}
+              dataKey="value"
+              nameKey="name"
+              innerRadius={60}
+              outerRadius={80}
+              fill="#82ca9d"
+              labeel
+            />
+          </PieChart>
         </div>
+        <div className="col-md-3"></div>
+        <div className="col-md-3"></div>
       </div>
     </>
   );
